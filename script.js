f0e75b35490f78ebc17857518857803248df5c37
Mason <masonbush751@gmail.com>
2021-03-24 16:29:22 -0500

    js functions

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('hi')
+const getWines = async () => {
+    let response = await fetch('http://myapi-profstream.herokuapp.com/api/21a11f/wines')
+    let data = await response.json()
+    console.log(data)
+}
+
+const addWine = async (name='', year=0, grapes='',country='', region='', description='', picture='', price=0) => {
+    let response = await fetch('http://myapi-profstream.herokuapp.com/api/21a11f/wines', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "name": name,
+            "year": year,
+            "grapes": grapes,
+            "country": country,
+            "region": region,
+            "description": description,
+            "picture": picture,
+            "price": price
+        })
+    })
+}

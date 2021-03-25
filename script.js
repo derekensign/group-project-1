34a155fde7ce3a5b152f6d1acbe267b45a3f665c
Mason <masonbush751@gmail.com>
2021-03-24 19:42:55 -0500

    First user story work

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
+document.querySelector('.allwines').addEventListener('click', () => {
+    getWines()
+})
+
+
+const required = varName => {
+    throw new Error(`${varName} is required. `);
+}
+
 const getWines = async () => {
     let response = await fetch('http://myapi-profstream.herokuapp.com/api/21a11f/wines')
     let data = await response.json()
-    console.log(data)
+
+    for(let i = 0; i < data.length; i++) {
+        let newDiv = document.createElement('div')
+        let newName = document.createElement('h5').innerHTML = data[i].name
+        let newImage = document.createElement('img').src = data[i].picture
+        let newYear = document.createElement('p').textContent = data[i].year
+        let newGrapes = document.createElement('p').textContent = data[i].grapes
+        let newCountry = document.createElement('p').textContent = data[i].country
+        let newRegion = document.createElement('p').textContent = data[i].region
+        let newDesc = document.createElement('p').textContent = data[i].description
+        let price = document.createElement('p').textContent = data[i].price
+        let elementArr = [newName,newImage,newYear,newGrapes,newCountry,newRegion,newDesc,price]
+        for (let element of elementArr) {
+            newDiv.append(element)
+        }
+        newDiv.classList.add("winediv")
+        document.querySelector('.wineinfo').append(newDiv)
+    }
 }
 
-const addWine = async (name='', year=0, grapes='',country='', region='', description='', picture='', price=0) => {
+const getWineById = async id => {
+    let response = await fetch(`http://myapi-profstream.herokuapp.com/api/21a11f/wines/${id}`)
+    let data = await response.json()
+}
+
+const addWine = async (name=required('name'), year=0, grapes='N/A',country='N/A', region='N/A', description='N/A', picture='N/A', price=0) => {
     let response = await fetch('http://myapi-profstream.herokuapp.com/api/21a11f/wines', {
         method: 'POST',
         headers: {
@@ -22,4 +53,12 @@ const addWine = async (name='', year=0, grapes='',country='', region='', descrip
         })
     })
 }
+
+const deleteWineById = async id => {
+    let response = await fetch(`http://myapi-profstream.herokuapp.com/api/21a11f/wines/${id}`, {
+        method: 'DELETE'
+    })
+}
+
+
 console.log('hello')
